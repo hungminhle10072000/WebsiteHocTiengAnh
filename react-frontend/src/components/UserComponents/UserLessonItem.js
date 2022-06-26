81a115cfb672a17a2bcd60b093a05846f43442b4
NghiaBui <nghiabv120100@gmail.com>
2022-06-26 21:56:42 +0700

    set id for voca topic, exercise

--- react-frontend/src/components/UserComponents/UserLessonItem.js
+++ react-frontend/src/components/UserComponents/UserLessonItem.js
@@ -8,7 +8,6 @@ import {
     AccountBookOutlined,
     CheckCircleOutlined
 } from '@ant-design/icons';
-import { TbVocabulary } from "react-icons/tb";
 
 import { Row, Col } from 'antd';
   
@@ -32,10 +31,9 @@ class UserLessonItem extends Component {
 
     render() {
         let hasExercise = this.props.lesson.exerciseId;
-        let hasVocabulary = this.props.lesson.vocabularyId;
-        let hasGrammar = this.props.lesson.grammarId;
+        let hasVocabulary = this.props.lesson.vocabularyTopicId;
         let isDoneExercise = this.props.lesson.doneExercise;
-        console.log("isDoneExercise",isDoneExercise)
+        // console.log("isDoneExercise",isDoneExercise)
         return(
             <div >
                 <Row className={!this.props.isSub ? 'lesson-item-disable' : ''}>
@@ -44,7 +42,7 @@ class UserLessonItem extends Component {
                             {isDoneExercise ? <CheckCircleOutlined style={{color:"green"}}/> : <div style={{width:"32px"}}></div>}
                         </div>
                     </Col>
-                    <Col span={19}>
+                    <Col span={21}>
                         <div style={{ padding: '0px' }} onClick={() => this.props.isSub && this.props.changedVideo(this.state.lesson.video, this.state.lesson.id, this.state.lesson.name)}>
                             <span style={{ fontWeight: 400 }}>{this.state.lesson.number}</span>
                             <img src="/svg/schedule_black_24dp.svg" alt="" height="15" style={{ marginRight: '5px' }} />
@@ -52,23 +50,16 @@ class UserLessonItem extends Component {
                             <hr style={{ marginBottom: '8px', marginTop: '8px', color: '#dddddd', height: '1px' }} />
                         </div>
                     </Col>
-                    <Col span={3}>
+                    <Col span={2}>
                         <Space >
-                            <Link className={this.props.isSub && hasExercise ? 'lesson-action lesson-action--enable' : 'lesson-action lesson-action--disable'} to={"/user/exercise/1"} style={{ textDecoration: 'none' }}>
+                            <Link className={this.props.isSub && hasExercise ? 'lesson-action lesson-action--enable' : 'lesson-action lesson-action--disable'} to={`/user/exercise/${hasExercise}`} style={{ textDecoration: 'none' }}>
                                 <Tooltip title="Bài tập">
                                     <FileDoneOutlined className='icon-css' />
                                 </Tooltip>
                             </Link>
-                            <Link className={this.props.isSub && hasVocabulary ? 'lesson-action lesson-action--enable' : 'lesson-action lesson-action--disable'} to={"/user/topic-vocabulary/1/shopping"} style={{ textDecoration: 'none', marginLeft: "0 !important" }}>
+                            <Link className={this.props.isSub && hasVocabulary ? 'lesson-action lesson-action--enable' : 'lesson-action lesson-action--disable'} to={`/user/topic-vocabulary/${hasVocabulary}/shopping`} style={{ textDecoration: 'none', marginLeft: "0 !important" }}>
                                 <Tooltip title="Từ vựng">
                                     <AccountBookOutlined className='icon-css' />
-                                </Tooltip>
-                            </Link>
-                            <Link className={this.props.isSub && hasGrammar ? 'lesson-action lesson-action--enable' : 'lesson-action lesson-action--disable'} to={"/user/grammar"} style={{ textDecoration: 'none' }}>
-                                <Tooltip title="Lý thuyết">
-                                    <span style={{ marginLeft: '16px !important' }}>
-                                        <TbVocabulary className='icon-css' />
-                                    </span>
                                 </Tooltip>
                             </Link>
                         </Space>
@@ -77,7 +68,6 @@ class UserLessonItem extends Component {
                     </Col>
                 </Row>
             </div>
-
         )
     }
 }
