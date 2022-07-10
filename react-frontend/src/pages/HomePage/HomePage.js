5181e666a52f3ac1ce88a2e26f466d42851f62a0
hungGit <hungduong.mess32@gmail.com>
2022-07-10 10:30:46 +0700

    add index for element in homepage

--- react-frontend/src/pages/HomePage/HomePage.js
+++ react-frontend/src/pages/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import './HomePage.css';
 import SliderSwipper from '../../components/SliderSwipper/SliderSwipper';
 import { connect } from 'react-redux';
 import allActions from '../../actions';
+import uuid from 'react-uuid';
 
 class HomePage extends Component {
 
@@ -39,17 +40,17 @@ class HomePage extends Component {
   render() {
     return (
       <Fragment>
-          <Slider />
+          <Slider key={uuid()}/>
           <div className="row mt-3">
             <div className='col-sm-12'>
                   <h2 className='label-course-new'>Khóa học mới</h2>
-                  <SliderSwipper dataCouseNew={this.state.listCourseNews}/>
+                  <SliderSwipper key={uuid()} dataCouseNew={this.state.listCourseNews}/>
             </div>
           </div>
           <div className='row mt-3'>
               <div className='col-md-12 mb-2'>
                   <h2 className='label-course-new'>Chủ đề mới</h2>
-                  <SliderSwipper dataTopicsNew={this.state.listTopicNews}/>
+                  <SliderSwipper key={uuid()} dataTopicsNew={this.state.listTopicNews}/>
               </div >
           </div>
       </Fragment>
